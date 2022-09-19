40a5ea82d7aa2527d67c3ddf9f636aa75bf593e4
parth <parthagravat99@gmail.com>
2022-09-19 14:47:37 +0530

    demo5 complete

---
+++ database/seeders/studentsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as faker;
+
+
+class studentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker=faker::create();
+        for($count=0;$count<10;$count++){
+            DB::table('students')->insert([
+                'name' => $faker->name(),
+                'email' => $faker->email(),
+                'phone'  => $faker->phoneNumber(),
+            ]);
+                
+    }
+}
+}
