13aad1d15fff7ff5d8f163098057b193a3db9f6a
parth <parthagravat99@gmail.com>
2022-10-07 15:56:49 +0530

    dingo install and configure

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->get('vishal', function(){
+        echo "vishal";
+    });
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
